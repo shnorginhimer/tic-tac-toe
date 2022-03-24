56fdda33771f93e86b840b5fcf1d1c46613de511
Aaron MacAulay <ajm.master7@gmail.com>
2022-03-23 18:28:22 -0700

    check for valid moves before doing them

--- tictactoe.js
+++ tictactoe.js
@@ -6,7 +6,7 @@ const board = [
 ];
 
 const players = ["X", " ", "O"];
-
+let lastPlayer = 0;
 function printBoard(b) {
   console.log(b[0].map((p) => players[p + 1]).join(" | "));
   console.log("---------");
@@ -17,9 +17,20 @@ function printBoard(b) {
 }
 
 function move(x, y, p) {
-  board[x - 1][y - 1] = p;
+  if (p == lastPlayer) {
+    console.log("be patient!!!");
+  } else if (p == 0) {
+    console.log("that's useless");
+  } else if (board[x - 1][y - 1] != 0) {
+    console.log("nice try");
+  } else {
+    // all good
+    board[x - 1][y - 1] = p;
+    lastPlayer = p;
+  }
 }
 
 printBoard(board);
 move(2, 2, -1);
+move(2, 1, -1);
 printBoard(board);
