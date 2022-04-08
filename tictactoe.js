f348e23193e355fd250897e0b8cc8305ec4ea11d
Aaron MacAulay <ajm.master7@gmail.com>
2022-04-07 21:03:52 -0700

    now check winner works for all cases

--- tictactoe.js
+++ tictactoe.js
@@ -10,7 +10,7 @@ let lastPlayer = 0;
 
 /**
  * prints board to console
- * @param {*} b 
+ * @param {*} b
  */
 function printBoard(b) {
   console.log(b[0].map((p) => players[p + 1]).join(" | "));
@@ -22,10 +22,10 @@ function printBoard(b) {
 }
 
 /**
- * 
- * @param {*} row 
- * @param {*} col 
- * @param {*} player 
+ *
+ * @param {*} row
+ * @param {*} col
+ * @param {*} player
  */
 function move(row, col, player) {
   if (player == lastPlayer) {
@@ -46,22 +46,40 @@ function move(row, col, player) {
 // checking if someone has won
 function checkWin(player) {
   console.log(`checking if ${player} has won...`);
+  // checking colunms
+  for (let c = 0; c < 3; c++) {
+    let sum = 0;
+    for (let r = 0; r < 3; r++) {
+      sum += board[r][c];
+    }
+    if (sum == 3 * player) return true;
+  }
+  // checking rows
+  for (let r = 0; r < 3; r++) {
+    let sum = 0;
+    for (let c = 0; c < 3; c++) {
+      sum += board[r][c];
+    }
+    if (sum == 3 * player) return true;
+  }
+  //checking diagonals
   let sum = 0;
-  for (let i = 0; i < 3; i++) {
-    sum += board[i][0];
+  for (let rc = 0; rc < 3; rc++) {
+    sum += board[rc][rc];
   }
-  if (sum == 3 * player) {
-    return true;
-  } else {
-    return false;
+  if (sum == 3 * player) return true;
+  sum = 0;
+  for (let rc = 0; rc < 3; rc++) {
+    sum += board[rc][2 - rc];
   }
+  if (sum == 3 * player) return true;
 }
 
 printBoard(board);
 move(3, 1, -1);
 move(2, 3, 1);
-move(2, 1, -1);
-move(3, 2, 1);
-move(1, 1, -1);
+move(3, 2, -1);
+move(1, 3, 1);
+move(3, 3, -1);
 move(1, 2, 1); // TODO noone should be able to move after someone has won
 printBoard(board);
