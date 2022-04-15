a272d115c01d544250bcf4914f5d78fe3bb2bfe7
Aaron MacAulay <ajm.master7@gmail.com>
2022-04-15 13:28:54 -0700

    started ui

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
 const { printBoard, setBoard, move, checkWin } = require("./tictactoe");
 
-setBoard([1, 1, 1], [0, 0, 0], [0, 0, 0]);
-printBoard();
-console.assert(checkWin(1));
-
-// TODO sandy change all these into tests that can all run without editing
-/*move(3, 1, -1);
-move(2, 3, 1);
-move(3, 2, -1);
-move(1, 3, 1);
-move(3, 3, -1);*/
-
-/*move(1, 1, -1);
-move(2, 3, 1);
-move(1, 2, -1);
-move(2, 2, 1);
-move(1, 3, -1);*/
-
-/*move(2, 1, -1);
-move(1, 3, 1);
-move(2, 2, -1);
-move(1, 2, 1);
-move(2, 3, -1);*/
-
 setBoard([0, 1, -1], [0, -1, 1], [-1, 0, 0]);
 printBoard();
 console.assert(checkWin(-1));
-//move(1, 3, -1);
-/*move(2, 3, 1);
-move(2, 2, -1);
-move(1, 2, 1);
-move(3, 1, -1);*/
-
-//move(1, 2, 1); // TODO none should be able to move after someone has won
--- spec/ttt.spec.js
+++ spec/ttt.spec.js
@@ -1,12 +1,31 @@
 const { printBoard, setBoard, move, checkWin } = require("../tictactoe");
 
 describe("Checking winners on various board setups", function () {
-    it("Checking that three Xs in a row win", function () {
-        setBoard([1, 1, 1], [0, 0, 0], [0, 0, 0]);
-        expect(checkWin(1)).toBe(true);
-    });
-    it("Checking that three Os on diagonal wins", function () {
-        setBoard([0, 1, -1], [0, -1, 1], [-1, 0, 0]);
-        expect(checkWin(-1)).toBe(true);
-    });
+  it("checks that three Xs in a row wins", function () {
+    setBoard([1, 1, 1], [0, 0, 0], [0, 0, 0]);
+    expect(checkWin(1)).toBe(true);
+  });
+  it("Checking that three Os on diagonal wins", function () {
+    setBoard([0, 1, -1], [0, -1, 1], [-1, 0, 0]);
+    expect(checkWin(-1)).toBe(true);
+  });
 });
+
+// TODO sandy change all these into tests that can all run without editing
+/*move(3, 1, -1);
+move(2, 3, 1);
+move(3, 2, -1);
+move(1, 3, 1);
+move(3, 3, -1);*/
+
+/*move(1, 1, -1);
+move(2, 3, 1);
+move(1, 2, -1);
+move(2, 2, 1);
+move(1, 3, -1);*/
+
+/*move(2, 1, -1);
+move(1, 3, 1);
+move(2, 2, -1);
+move(1, 2, 1);
+move(2, 3, -1);*/
