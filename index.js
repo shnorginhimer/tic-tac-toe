100a45324f421858fcc06369c41579c39cc91f7f
Alex MacAulay <macaulay.sandy@gmail.com>
2022-05-15 18:56:08 -0700

    pair programming with Aaron

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const {
   move,
   checkWin,
   htmlBoard,
+  userMove
 } = require("./tictactoe");
 
-setBoard([0, 1, -1], [0, -1, 1], [-1, 0, 0]);
+setBoard([0, 1, 1], [0, 0, 0], [-1, 0, 0]);
+
+userMove(2, 2);
 printBoard();
 console.log(htmlBoard());
-console.assert(checkWin(-1));
+// console.assert(checkWin(-1));
--- tictactoe.js
+++ tictactoe.js
@@ -4,15 +4,22 @@ const board = [
   [0, 0, 0],
   [0, 0, 0],
 ];
+const players = ["O", " ", "X"];
+const USER = 1
+const AI = -1;
+let lastPlayer = 0;
 
+/**
+ * Setup the board to a specific starting position
+ * @param {*} r1 
+ * @param {*} r2 
+ * @param {*} r3 
+ */
 function setBoard(r1, r2, r3) {
   board[0] = r1;
   board[1] = r2;
   board[2] = r3;
 }
-
-const players = ["O", " ", "X"];
-let lastPlayer = 0;
 
 /**
  * prints board to console
@@ -28,7 +35,7 @@ function printBoard() {
 }
 
 /**
- * prints board to console
+ * converts board into the body of an HTML table with the needed events hooked up
  * @param {*} b
  */
 function htmlBoard() {
@@ -36,13 +43,10 @@ function htmlBoard() {
   let tableContents = "";
 
   // One row at a time
+  // for each (row,col) position generate the needed <td onclick="<function>"">X or O</td>
   for (let row = 0; row < 3; row++) {
     tableContents +=
-      ("\n<tr>" +
-        board[row].map( // for each (row,col) position generate the needed <td onclick="<function>"">X or O</td>
-          (currPlayer, col) => `<td onclick="${makeMoveCall(row, col, currPlayer)
-            }">${players[currPlayer + 1]}</td>`
-        ).join(" ") +
+      ("\n<tr>" + board[row].map((currPlayer, col) => `<td onclick="${makeMoveCall(row, col, currPlayer)}">${players[currPlayer + 1]}</td>`).join(" ") +
         "</tr>");
   }
 
@@ -70,6 +74,35 @@ function makeMoveCall(row, col, currentPlayer) {
   } else {
     return "alert('already taken')";
   }
+}
+
+/**
+ * Accepts a move from the user and applies it
+ * Then gets the AI to make a move in repsonse
+ * @param {*} row 
+ * @param {*} col 
+ */
+function userMove(row, col) {
+  move(row, col, USER);
+  const [aiRow, aiCol] = aiDecideMove();
+  move(aiRow, aiCol, AI);
+  console.log(`user moved to ${row}, ${col}`);
+}
+
+/**
+ * Figure out what move the AI should make
+ * @returns 
+ */
+function aiDecideMove() {
+  // Check if user about to win
+  const neededMove = checkAboutToWin(USER);
+  if (neededMove) return neededMove;
+
+  // If not make a random move
+  const row = Math.floor(Math.random() * 3) + 1;
+  const col = Math.floor(Math.random() * 3) + 1;
+  console.log(`AI moving to ${row}, ${col}`);
+  return [row, col];
 }
 
 /**
@@ -111,6 +144,33 @@ function checkWin(player) {
   return false;
 }
 
+/**
+ * Check if a player is about to win
+ * Return the first location that will win so you can move there instead
+ * @param {*} player 
+ * @returns 
+ */
+function checkAboutToWin(player) {
+  console.log(`checking if player "${players[player + 1]}" is about to win...`);
+  // checking colunms
+  for (let c = 0; c < 3; c++)
+    if (calcSum(0, 1, c, 0) == 2 * player) {
+      for (let r = 0; r < 3; r++) {
+        if (board[r, c] == 0) return [r, c]
+      }
+      console.error("oops shouldn't get here");
+    }
+
+  // checking rows
+  for (let r = 0; r < 3; r++)
+    if (calcSum(r, 0, 0, 1) == 2 * player) return [1, 1];
+
+  //checking diagonals
+  if (calcSum(0, 1, 0, 1) == 2 * player) return [1, 1];
+  if (calcSum(0, 1, 2, -1) == 2 * player) return [1, 1];
+  return [3, 3];
+}
+
 function calcSum(row, rowDelta, col, colDelta) {
   //console.log("row", "col");
   let sum = 0;
@@ -126,5 +186,6 @@ module.exports = {
   htmlBoard: htmlBoard,
   setBoard: setBoard,
   move: move,
+  userMove: userMove,
   checkWin: checkWin,
 };
